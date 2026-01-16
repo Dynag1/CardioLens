2ac5c19bf5f458206642beb4be16586b4646efa5
hemge <contact@dynag.co>
2026-01-16 17:48:12 +0100

    chore: Bump version to 1.1.8 for Play Store Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cardio.fitbit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.1.7"
+        versionCode = 9
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
