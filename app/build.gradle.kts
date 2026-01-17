855ae106f76d818c015821c95f4f62fb088c1bd0
hemge <contact@dynag.co>
2026-01-17 15:52:03 +0100

    Fix: Scientifically improved RHR calculation (Median/Average)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cardio.fitbit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1.11"
+        versionCode = 12
+        versionName = "1.1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
