ebf4a89f009793504e568e23f057cca0d23bf1b4
hemge <contact@dynag.co>
2026-01-22 16:34:49 +0100

    Bump version to 1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cardio.fitbit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.2.1"
+        versionCode = 31
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
