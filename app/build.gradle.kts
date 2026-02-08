1335962c30ce4935d78d967e2294ac9c81f64ac1
hemge <contact@dynag.co>
2026-02-08 16:31:32 +0100

    Bump version to 1.3.3: Fixed Health Connect duplicates and missing Pace/Steps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cardio.fitbit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 62
-        versionName = "1.3.2"
+        versionCode = 63
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
