8fc10ffdf52454bb855d68209c0e1dca7f7464f1
hemge <contact@dynag.co>
2026-02-08 12:20:39 +0100

    Bump version to 1.3.0: Fixed Restore Crash with batched inserts & improved error handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cardio.fitbit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 58
-        versionName = "1.2.28"
+        versionCode = 60
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
