f250132c547e2ea5c1df0529de20901895639a6a
hemge <contact@dynag.co>
2026-02-12 11:53:19 +0100

    chore: Bump version code to 77 for v1.3.17 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cardio.fitbit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 76
-        versionName = "1.3.16"
+        versionCode = 77
+        versionName = "1.3.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
