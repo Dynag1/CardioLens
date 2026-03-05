a024d03609ac1859589ad280ca509379a23df8f8
Votre Nom <votre.email@exemple.com>
2026-03-05 18:52:02 +0100

    Bump version to 1.3.42 (92)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cardio.fitbit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 91
-        versionName = "1.3.41"
+        versionCode = 92
+        versionName = "1.3.42"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
